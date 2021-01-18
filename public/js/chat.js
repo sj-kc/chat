797f38ba732d294eee8cc7eaa17a6a86f6dadd34
S.Kim Castro <s.kim.castro@gmail.com>
2021-01-18 21:24:28 +0200

    Ui fixes

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,6 +17,24 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
+const autoScroll = () => {
+  const $newMessage = $messages.lastElementChild;
+
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+  const visibleHeight = $messages.offsetHeight;
+
+  const containerHeight = $messages.scrollHeight;
+
+  const scrollOffset = $messages.scrollTop + visibleHeight;
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
+
 socket.on("message", ({ message, createdAt, username }) => {
   const html = Mustache.render(messageTemplate, {
     message,
@@ -24,6 +42,7 @@ socket.on("message", ({ message, createdAt, username }) => {
     username,
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoScroll();
 });
 
 socket.on("locationMessage", ({ url, createdAt, username }) => {
@@ -33,6 +52,7 @@ socket.on("locationMessage", ({ url, createdAt, username }) => {
     username,
   });
   $messages.insertAdjacentHTML("beforeend", html);
+  autoScroll();
 });
 
 socket.on("roomData", ({ room, users }) => {
